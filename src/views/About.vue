63076640aee09c212159454be7b794b6fcbd661d
Cherrie <cherriekim925@gmail.com>
2023-05-13 14:31:17 +0900

    [Feat] About, Todo Page 생성. Image 파일 수정

---
+++ src/views/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, dolorem
+      sequi totam, ad aperiam dolorum, ratione nihil doloremque nostrum omnis
+      sapiente error sunt consequuntur amet explicabo doloribus! Ipsum, nobis
+      iure.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, dolorem
+      sequi totam, ad aperiam dolorum, ratione nihil doloremque nostrum omnis
+      sapiente error sunt consequuntur amet explicabo doloribus! Ipsum, nobis
+      iure.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, dolorem
+      sequi totam, ad aperiam dolorum, ratione nihil doloremque nostrum omnis
+      sapiente error sunt consequuntur amet explicabo doloribus! Ipsum, nobis
+      iure.
+    </p>
+  </div>
+</template>
