27d20783febe8e34ebfcea7468a148dd3c756d1b
Cherrie <cherriekim925@gmail.com>
2023-05-13 15:17:25 +0900

    [Feat] 체크 여부에 따라 상태 변환. 상태에 따라 생김새 변환.

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -2,18 +2,27 @@
 <template>
   <div class="home">
     <!--Vuetify 라이브러리에서 제공하는 v-list. flat attribute은 아무런 그림자나 elevation 없음을 의미-->
-    <v-list class="pt-0" flat>
+    <v-list class="pt-1" flat>
       <div v-for="task in tasks" :key="task.id">
-        <v-list-item>
+        <v-list-item
+          @click="doneTask(task.id)"
+          :class="{
+            'blue lighten-5':
+              task.done /* task.done이 true일 때만 blue property를 가짐*/,
+          }"
+        >
           <!--v-slot:default로 컨텐트 분배를 위한 slot 정의함. 
         이 slot의 이름은 'default'로 하고, 이건 'active'라는 프로퍼티를 가진 data object 받음-->
-          <template v-slot:default="{ active }">
+          <template v-slot:default>
             <v-list-item-action>
-              <!--:input-value 속성은 체크박스의 값과 슬롯에 전달된 데이터 객체의 'active' 속성 바인딩함-->
-              <v-checkbox :input-value="active" color="primary"></v-checkbox>
+              <!--:input-value 속성이 true일 때 체크함-->
+              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
             </v-list-item-action>
             <v-list-item-content>
-              <v-list-item-title>{{ task.title }}</v-list-item-title>
+              <v-list-item-title
+                :class="{ 'text-decoration-line-through': task.done }"
+                >{{ task.title }}</v-list-item-title
+              >
             </v-list-item-content>
           </template>
         </v-list-item>
@@ -30,11 +39,22 @@ export default {
   data() {
     return {
       tasks: [
-        { id: 1, title: "wake up" },
-        { id: 2, title: "eat oatmeal" },
-        { id: 3, title: "morning workout" },
+        { id: 1, title: "wake up", done: false },
+        { id: 2, title: "eat oatmeal", done: false },
+        { id: 3, title: "morning workout", done: false },
       ],
     };
+  },
+
+  methods: {
+    doneTask(id) {
+      // 전달받은 id와 일치하는 task 찾음
+      // fiter는 iteration을 돌며 주어진 조건 (task.id === id)와 만족하는 task를 찾는다
+      let task = this.tasks.filter((task) => task.id === id)[0];
+      // 주의해야 할 점: task는 단일 object가 아니라 array of object를 반환하기 때문에, 꼭 [0]를 붙여서 하나만 반환하게 해야함
+
+      task.done = !task.done;
+    },
   },
 };
 </script>
