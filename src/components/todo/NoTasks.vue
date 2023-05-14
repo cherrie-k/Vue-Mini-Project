7d58100e387a54f31d8bd87ab6d7540e60c44ada
Cherrie <cherriekim925@gmail.com>
2023-05-15 00:00:44 +0900

    [UI] about page UI 업데이트

--- src/components/todo/NoTasks.vue
+++ src/components/todo/NoTasks.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="no-task">
+  <div class="no-task d-flex flex-column align-center">
     <v-icon size="100" color="pink lighten-2">mdi-check</v-icon>
     <div class="text-h5 pink--text">No tasks!</div>
+    <div class="text-caption pink--text">화면을 작게도 해보세요 :)</div>
   </div>
 </template>
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,23 +1,76 @@
 <template>
   <div class="about pa-6">
-    <h1>About Vuetify Todo</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, dolorem
-      sequi totam, ad aperiam dolorum, ratione nihil doloremque nostrum omnis
-      sapiente error sunt consequuntur amet explicabo doloribus! Ipsum, nobis
-      iure.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, dolorem
-      sequi totam, ad aperiam dolorum, ratione nihil doloremque nostrum omnis
-      sapiente error sunt consequuntur amet explicabo doloribus! Ipsum, nobis
-      iure.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, dolorem
-      sequi totam, ad aperiam dolorum, ratione nihil doloremque nostrum omnis
-      sapiente error sunt consequuntur amet explicabo doloribus! Ipsum, nobis
-      iure.
-    </p>
+    <h2>In This Vuetify Todo...</h2>
+    <h3>YOU CAN</h3>
+    <template>
+      <v-container fluid>
+        <p class="gray--text">{{ selected }}</p>
+        <v-checkbox
+          class="mb-n4"
+          v-model="selected"
+          label="Add, delete, edit todos"
+          value="Add, delete, edit"
+          color="red lighten-1"
+        ></v-checkbox>
+        <v-checkbox
+          class="mb-n4"
+          v-model="selected"
+          label="Set due dates"
+          value="Set date"
+          color="red lighten-2"
+        ></v-checkbox>
+        <v-checkbox
+          class="mb-n4"
+          v-model="selected"
+          label="Have data saved at local DB"
+          value="save @ local DB"
+          color="red lighten-3"
+        ></v-checkbox>
+        <v-checkbox
+          class="mb-n4"
+          v-model="selected"
+          label="Good on both mobile and PC screen"
+          value="mobile & PC"
+          color="red lighten-4"
+        ></v-checkbox>
+        <v-checkbox
+          class="mb-n4"
+          v-model="selected"
+          label="Reorder todos"
+          value
+          disabled
+        ></v-checkbox>
+        <v-checkbox
+          class="mb-n4"
+          v-model="selected"
+          label="Search todos"
+          value
+          disabled
+        ></v-checkbox>
+        <v-checkbox
+          class="mb-n4"
+          v-model="selected"
+          label="Display live date and time"
+          value
+          disabled
+        ></v-checkbox>
+      </v-container>
+    </template>
+    <p>And more is yet to come...!</p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      selected: [
+        "Add, delete, edit",
+        "Set date",
+        "save @ local DB",
+        "mobile & PC",
+      ],
+    };
+  },
+};
+</script>
